623321b4cb1c867652e63596f6bdc855ad28c81e
Chris Villarreal <chrisvillarreal1018@gmail.com>
2013-04-13 10:43:49 -0500

    saving work on home.html

--- caseword/urls.py
+++ caseword/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from caseword import views
+from caseword import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,3 +10,6 @@ urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
